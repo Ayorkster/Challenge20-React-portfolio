2f30d42813af37ffddab623d7e6a923e1e644874
mono <traineralaina@gmail.com>
2023-12-02 15:04:19 -0700

    added icons and css styling'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Github from "../images/github-mark.png";
+import linkedin from "../images/LI-in-Bug.png"
 
 const Footer = () => {
   return (
-    <footer>
-
-      <p id="copyright">&copy; 2023</p>
+    <footer id="copyright">
+ <a href="https://github.com/ayorkster" target="_blank" rel="noopener noreferrer">
+        <img className="contact" src= { Github } alt="github logo" />
+      </a>
+      <a href="https://www.linkedin.com/in/alaina-york-7a1567243/" target="_blank" rel="noopener noreferrer">
+        <img className="contact" src= { linkedin } alt="github logo" />
+      </a>
+      <p>&copy; 2023</p>
     </footer>
   );
 };
